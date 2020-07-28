e2e763712f46a44f9f8e65112445f4aabd4804b0
tenzin <ten13zin@gmail.com>
2020-07-28 13:38:54 +0530

    add: compute diff on text1 and text2

--- dmp.js
+++ dmp.js
@@ -1,17 +1,36 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var program = require('commander');
+var dmp_module = require("./diff_match_patch.js");
 
-function diff(src_path, dest_path) {
-  var src_text = fs.readFileSync(src_path, 'utf8');
-  var dest_text = fs.readFileSync(dest_path, 'utf8');
-  console.log(src_text);
-  console.log(dest_text)
+var dmp = new dmp_module.diff_match_patch();
+
+function log_list_of_tuples(diffs) {
+  // log diffs in list of tuple like
+  // [(-1, "Hell"), (1, "G"), (0, "o"), (1, "odbye"), (0, " World.")]
+  console.log('[')
+  for (var d of diffs) {
+    console.log('(' + d[0] + ', ' + '"' + d[1] + '"' + '),');
+  }
+  console.log(']')
+}
+
+function diff(text1_path, text2_path) {
+  // read srouce and destination text
+  var text1 = fs.readFileSync(text1_path, 'utf8');
+  var text2 = fs.readFileSync(text2_path, 'utf8');
+
+  // find the diff of source to destination text.
+  var diffs = dmp.diff_main(text1, text2);
+
+  // stdout the diffs
+  log_list_of_tuples(diffs)
 }
 
 program
   .version('0.0.1')
-  .command('diff <src_path> <dest_path>')
+  .command('diff <text1_path> <text2_path>')
+  .description('Compute diff between text1 and text2')
   .action(diff);
 
 program.parse(process.argv);
